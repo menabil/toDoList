737ced7ac8c22d3e7e111ca75000f24b0ce854fc
menabil <nabil99cc@gmail.com>
2026-05-23 00:29:51 +0600

    some edit css & js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,34 @@
 const inputBox = document.getElementById("inputBox");
 const todoList = document.getElementById("todoList");
 
-function addTask() {
-  if (inputBox.value === "") {
+let addTask = () => {
+  if (inputBox.value == "") {
     alert("please add a task");
   } else {
     let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
+    li.innerText = inputBox.value;
     todoList.appendChild(li);
     inputBox.value = "";
     let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
+    span.innerText = "x";
     li.appendChild(span);
     saveData();
   }
-}
+};
 
-todoList.addEventListener(
-  "click",
-  function (e) {
-    if (e.target.tagName === "SPAN") {
-      e.target.parentElement.remove();
-      saveData();
-    }
-  },
-  false,
-);
+todoList.addEventListener("click", function (e) {
+  if (e.target.tagName == "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+});
 
-function saveData() {
-  localStorage.setItem("data", todoList.innerHTML);
-}
+let saveData = () => {
+  localStorage.setItem("eoo", todoList.innerHTML);
+};
 
-function showTask() {
-  todoList.innerHTML = localStorage.getItem("data");
-}
+let showTask = () => {
+  todoList.innerHTML = localStorage.getItem("eoo");
+};
 
 showTask();
